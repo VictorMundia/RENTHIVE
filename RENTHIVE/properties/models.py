49f3cd0c719fd7f09dc0fbdb60b76b3f56d05545
Victor <victor.mundia@strathmore.edu>
2025-06-12 12:14:59 +0300

    Register Models in Admin

--- RENTHIVE/properties/models.py
+++ RENTHIVE/properties/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Property(models.Model):
-    owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.TextField()
     county = models.CharField(max_length=100)
     landmark = models.CharField(max_length=100)
