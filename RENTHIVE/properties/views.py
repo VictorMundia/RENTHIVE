97623eb76ae570abf6a35df241dc79415c3854fe
Victor <victor.mundia@strathmore.edu>
2025-06-12 12:43:10 +0300

    Urls

--- RENTHIVE/properties/views.py
+++ RENTHIVE/properties/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Property
 
 # Create your views here.
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, 'properties/property_list.html', {'properties': properties})
--- RENTHIVE/propertyowner/models.py
+++ RENTHIVE/propertyowner/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
-
-from django.db import models
+from django.conf import settings
 
 class PropertyOwner(models.Model):
-    full_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(unique=True)
-    password_hash = models.CharField(max_length=256)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True  # Makes this a true profile model
+    )
+    # full_name can be derived from user.get_full_name() or user.first_name and user.last_name
+    phone = models.CharField(max_length=20, unique=True, null=True, blank=True) # Phone specific to the owner profile
+    # email is already part of settings.AUTH_USER_MODEL (user.email)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.full_name
+        return self.user.get_full_name() or self.user.username
